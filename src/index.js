9567372b04fc5535b5220e7842a2e399f8dbe273
ambrittc <amandabchung@gmail.com>
2022-09-14 12:27:12 -0400

    Weather API

--- src/index.js
+++ src/index.js
@@ -23,25 +23,37 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
     forecastHTML =
       forecastHTML +
       `<div class="col-2">
-        <div class="weather-forecast-date">
-        ${day}
+        <div class="weather-forecast-date">${formatDay}
+        ${forecastDay.dt}
         </div>
-        <img src="https://openweathermap.org/img/wn/50d@2x.png" alt=" " class="pics" />
+        ${index}
+        <img src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt=" " class="pics" />
         <div class="weather-forecast-temperature">
           <span class="weather-forecast-temperature-max">
-            <strong> 22° </strong></span>
+            <strong> ${Math.round(forecastDay.temp.max)}° </strong></span>
             <span class="weather-forecast-temperature-min">
-            16° </span>
+            ${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>`;
   });
